87b1fcb5902041a75682f7ab2888d434d14ee7ce
6br <tfdvrt@gmail.com>
2020-03-08 11:27:31 +0900

    Support --tlsv1.2

--- cargo.sh
+++ cargo.sh
@@ -4,6 +4,6 @@ export CACHE_DIR=`pwd`/cargo
 export CARGO_HOME=$CACHE_DIR
 export RUSTUP_HOME=$CACHE_DIR
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal  --default-toolchain nightly
+curl --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal  --default-toolchain nightly
 
 `pwd`/cargo/bin/cargo build --release
