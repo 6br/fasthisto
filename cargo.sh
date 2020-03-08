2f7ffbdeb8bda4b00e567257f21010f43886bb6d
6br <tfdvrt@gmail.com>
2020-03-08 12:40:31 +0900

    Update cargo

--- cargo.sh
+++ cargo.sh
@@ -4,6 +4,6 @@ export CACHE_DIR=`pwd`/cargo
 export CARGO_HOME=$CACHE_DIR
 export RUSTUP_HOME=$CACHE_DIR
 
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal  --default-toolchain nightly
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y --profile minimal --default-toolchain nightly
 
 `pwd`/cargo/bin/cargo build --release
