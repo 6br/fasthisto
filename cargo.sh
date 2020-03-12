4b69ffe41c9fd7e6a3166d27af7a75a3c57b8c80
6br <tfdvrt@gmail.com>
2020-03-12 20:45:38 +0900

    Update cargo

--- cargo.sh
+++ cargo.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 export CACHE_DIR=`pwd`/cargo
 export CARGO_HOME=$CACHE_DIR
@@ -6,4 +6,14 @@ export RUSTUP_HOME=$CACHE_DIR
 
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y --profile minimal --default-toolchain nightly
 
-`pwd`/cargo/bin/cargo build --release
+mkdir .cargo
+
+target=`rustup show active-toolchain`
+
+if [[ ${target} =~ ^[^-]*-(.*)\ .*$ ]]; then
+  echo -e "[target.${BASH_REMATCH[1]}]\nlinker = '$cc'" > .cargo/config
+fi
+
+
+`pwd`/cargo/bin/cargo build --release # C-compiler, library path, 
+
